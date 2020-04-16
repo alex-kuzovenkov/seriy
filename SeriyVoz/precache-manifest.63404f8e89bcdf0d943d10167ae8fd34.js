f98ac8d0ccab1ca1dee8a6bad02a7880e36f9acd
alex-kuzovenkov <58694587+alex-kuzovenkov@users.noreply.github.com>
2020-04-16 23:47:22 +0300

    a

---
+++ SeriyVoz/precache-manifest.63404f8e89bcdf0d943d10167ae8fd34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7744d8e44be16ccb5bf33d6cdef3607",
+    "url": "/Seriy/index.html"
+  },
+  {
+    "revision": "69aeb9ce3e5f6bbed8da",
+    "url": "/Seriy/static/css/main.6c288863.chunk.css"
+  },
+  {
+    "revision": "69b7e40d6059733ce538",
+    "url": "/Seriy/static/js/2.ea32ffd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Seriy/static/js/2.ea32ffd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69aeb9ce3e5f6bbed8da",
+    "url": "/Seriy/static/js/main.088f585e.chunk.js"
+  },
+  {
+    "revision": "c33ea2e61914aeec76d5",
+    "url": "/Seriy/static/js/runtime-main.f8d5c842.js"
+  }
+]);
